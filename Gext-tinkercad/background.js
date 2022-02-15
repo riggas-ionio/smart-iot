224d1b1537e210e548f5cbd4b683296c139da9d1
Dimitrios Ringas <riggas@ionio.gr>
2022-02-15 10:01:09 +0200

    Welcome 2022

--- Gext-tinkercad/background.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    if (tab.url.indexOf("https://www.tinkercad.com/things/") != -1) {
-        chrome.tabs.executeScript(tab.id, { "file": "lib/jquery-3.5.1.min.js" }, function() {
-            chrome.tabs.executeScript(tab.id, { "file": "contentScript.js" });
-        });
-    }
-});
