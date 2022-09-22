490e59b2c26be6d537d9a824ec3d025bcd78fd03
Dimitrios Ringas <55480871+riggas-ionio@users.noreply.github.com>
2022-09-22 19:01:38 +0300

    Update coop.js

--- it/coop.js
+++ it/coop.js
@@ -1,1 +1,6 @@
+console.log('Hacking coop :-)');
+var nextmonth = document.querySelector("#calendar > table > tbody > tr:nth-child(1) > td:nth-child(3) > div”);
+nextmonth.click();
+var mydate = document.querySelector("#calendar > table > tbody > tr:nth-child(6) > td:nth-child(5) > div”);
+mydate.click();
 
